8ee893f24f58a6dfc79cf463d2aa78cc0988df0d
QQ-War <2991889+QQ-War@users.noreply.github.com>
2025-12-16 14:23:02 -0500

    Add missing weight import in BookListScreen

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -86,6 +86,8 @@ dependencies {
     implementation("androidx.media3:media3-session:1.3.1")
     implementation("androidx.media:media:1.6.0")
 
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
