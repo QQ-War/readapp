7acd766d549c3e9e4428f3a42cdd12385e6787a5
QQ-War <2991889+QQ-War@users.noreply.github.com>
2025-12-18 00:04:31 -0500

    Enable minification for release build type

--- androidapp2/build.gradle.kts
+++ androidapp2/build.gradle.kts
@@ -32,12 +32,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
         }
         create("unsigned") {
             initWith(getByName("release"))
